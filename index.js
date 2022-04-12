d0b02ca92c26889a1f45a87238954dd62ae1812a
JacobRosey <jrosey77@gmail.com>
2022-04-12 11:57:56 -0400

    init

---
+++ index.js
@@ -0,0 +1,191 @@
+//Run with npm start
+
+const PORT = 8000
+
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs')
+
+const app = express()
+app.use(cors())
+
+const schedule = 'https://www.trinethunder.com/sports/sball/2021-22/schedule'
+const news = 'https://www.trinethunder.com/sports/sball/2021-22/news'
+const stats = 'https://www.trinethunder.com/sports/sball/2021-22/players/adrienneroseybff7'
+//app.METHOD(PATH, HANDLER)
+app.get('/record', (req, res) => {
+    axios(schedule)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const recordArr = []
+
+            $('.clearfix .cat .value', html).each(function(){
+                var records = $(this).text()
+                recordArr.push(records)
+            })
+            res.json(recordArr)
+        }).catch(err => console.log(err))
+})
+
+app.get('/stats', (req, res) => {
+    axios(stats)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const statDesc = []
+        const statNum = []
+        const statArr = [statDesc, statNum]
+
+        $('.stat-title', html).each(function(){
+            var stat1 = $(this).text()
+            statDesc.push(stat1)
+        })
+        $('.stat-value', html).each(function(){
+            var stat2 = $(this).text()
+            statNum.push(stat2)
+        })
+        res.json(statArr)
+    }).catch(err => console.log(err))
+})
+
+
+app.get('/schedule', (req, res) => {
+
+axios(schedule)
+.then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    const gameDate = []
+    const gameOpp = []
+    const gameStatus = []
+    const gameResult = []
+    const games = [gameDate, gameOpp, gameStatus]
+
+    //Find all elements of given class names within each event row element
+    $('.e_date', html).each(function(){
+        var date = $(this).text()
+        gameDate.push(date)
+    })
+    $('.team-name', html).each(function(){
+        var opponent = $(this).text()
+        gameOpp.push(opponent)
+    })
+    $('.e_status', html).each(function(){
+        var status = $(this).text()
+        gameStatus.push(status)
+    })
+    $('.e_result', html).each(function (){
+        var result = $(this).text()
+        gameResult.push(result)
+    })
+
+    schedApp.cleanData(games,gameResult);
+    res.json(games)
+    }).catch(err => console.log(err))
+})
+
+app.get('/news', (req, res) => {
+
+axios(news)
+.then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    const newsHeadline = []
+    const newsDate = []
+    const newsSrc = []
+    const articles = [newsHeadline, newsDate, newsSrc]
+
+    //Only grabbing the 5 most recent articles
+    $('.title',html).each(function (){
+        const headline = $(this).text()
+        if(newsHeadline.length < 5){
+            newsHeadline.push(headline)
+        }
+    })
+    $('.details .date', html).each(function(){
+        const date = $(this).text()
+        if(newsDate.length < 5){
+            newsDate.push(date)
+        }
+    })
+    $('.title-box', html).each(function(){
+        const src = $(this).attr('href')
+        if(newsSrc.length < 5){
+            newsSrc.push("https://trinethunder.com/"+src)
+        } 
+    }) 
+    newsApp.logNews(articles)
+    res.json(articles)
+}).catch(err => console.log(err))
+})
+
+app.listen(PORT , () => console.log(`server running on PORT ${PORT}`))
+
+var schedApp = new function(){
+    this.cleanData = function(data, result){
+        //Iterate through 2d array to access all data
+        for(let i=0; i<data.length; i++){
+            for(let j=0; j<data[i].length; j++){
+                //If current array is date array
+                if(i===0){
+                    //Delete period, spaces from month abbrev.
+                    data[i][j] = data[i][j].replace(/[ .]/g, "")
+
+                    //Remove zero if not followed by end of string
+                    data[i][j] = data[i][j].replace(/0(?!$)/, '')
+
+                    //Every other date is blank, but here I set blanks to the same date
+                    //as the last game. If the date is blank, it's played on same 
+                    //date as last game. check Trine schedule to see what I mean
+                    if(data[i][j].length == 1){
+                        data[i][j] = data[i][j-1]
+                    }
+                    
+                }
+                if(i===2){
+                    //Replace all new lines, spaces and tab spaces
+                    //data[i][j] = data[i][j].replace(/[ \t\n]/g, "")
+                    if(data[i][j].includes("Final")){
+                        data[i][j] = result[j]
+                    } 
+                    if(data[i][j].includes("Top" || "Bottom")){
+                        data[i][j] = result[j] + " " + data[i][j]
+                    }
+                }
+            }
+        } 
+    }
+
+}
+
+var newsApp = new function(){
+    var archiveArr = [];
+    this.logNews = function(articles){
+        for(let i=0; i<articles.length; i++){
+            for(let j=0; j<articles[i].length; j++){
+                console.log(articles[i][j] + "\n")
+                /*if(i == 0){
+                    if(articles[i][j].includes("Rosey")){
+                        archiveArr += articles[0][j] + articles[1][j] + articles[2][j]
+                    }
+                }*/
+            }
+        }
+        /*
+        let content = '';
+        content = archiveArr[0] + archiveArr[1] + archiveArr[2]
+        if('/Users/jacobrosey/Desktop/Web Dev Projects/Trine Reminder/archive.txt'.includes(!content)){
+            fs.writeFile('/Users/jacobrosey/Desktop/Web Dev Projects/Trine Reminder/archive.txt', content, { flag: 'a+' }, err =>
+            {
+                if(err){
+                    console.error(err)
+                    return
+                }
+    
+            }) 
+        }*/
+    }
+}
---
+++ src/app.js
@@ -0,0 +1,113 @@
+const schedDisplay = document.querySelector('#feed')
+const dateDisplay = document.querySelector('#dates')
+const oppDisplay = document.querySelector('#opponents')
+const statusDisplay = document.querySelector('#status')
+const newsDisplay = document.querySelector('#news')
+const recDisplay = document.querySelector('#record')
+const statDisplay = document.querySelector('#stats')
+
+//Append the current team record for conference, overall, and win or loss streak
+fetch('http://localhost:8000/record')
+    .then(response => {return response.json()})
+    .then(data => {
+        let overall =  `<h3 id="overall-rec">Overall Record: `+data[0]+`</h3>`
+        let conf = `<h3 id="conf-rec">Conference Record: `+data[2]+`</h3>`
+        let streak = `<h3 id="streak">Current Streak: `+data[4]+`</h3>`
+
+        recDisplay.innerHTML = overall + conf + streak
+    })
+
+//Append my sister's current stats
+fetch('http://localhost:8000/stats')
+    .then(response => {return response.json()})
+    .then(data => {
+        console.log(data)
+        for(let i=0; i<data[0].length; i++){
+            var statDesc = `<p id="stat-desc">`+data[0][i].toUpperCase()+`</p>`   
+            statDisplay.innerHTML += statDesc
+        }
+        for(i=0; i<data[1].length; i++){
+            var statNum = `<p id="stat-num">`+data[1][i]+`</p>`
+            statDisplay.innerHTML += statNum
+        }
+             
+    }).catch(err=>console.log(err))
+
+//Append the 5 most recent news articles about the team
+fetch('http://localhost:8000/news')
+    .then(response => {return response.json()})
+    .then(data => {
+        console.log(data)
+        
+        for(let i=0; i<data[0].length; i++){
+            let headline = `<h3 id="headline">`+ data[0][i] +`</h3>`
+            let date =    `<p id="news-date">`+ data[1][i] +`</p>`
+            let link =    `<a id="src" target="_blank" href="`+ data[2][i]+
+                               `"> Click Here to Read More</a><br><br>`
+            //newsDisplay.insertAdjacentHTML("beforeend", storyItem)
+            newsDisplay.innerHTML += headline + date + link
+        }
+    }).catch(err=>console.log(err))
+
+//Append the schedule for the entire season
+fetch('http://localhost:8000/schedule')
+    .then(response => {return response.json()})
+    .then(data => {
+        //Uncomment to see Active Game Alert
+        //data[2][40] = "Top of 5th"
+        for(let i=0; i<data[0].length; i++){
+            //If i is even, #ccc background, if i is odd, white background
+            var oddeven = ''
+            if(i % 2 == 0){
+                oddeven = 'even'
+            } else  oddeven = 'odd'
+
+            //Change dates back to more readable format now that comparisons are finished
+            if(data[0][i].includes("Mar")){
+                data[0][i] = data[0][i].replace('Mar', 'March ')
+            }
+            if(data[0][i].includes("Apr")){
+                data[0][i] = data[0][i].replace('Apr', 'April ')
+            }
+            if(data[0][i].includes("May")){
+                data[0][i] = data[0][i].slice(0, 3)+ " " + data[0][i].slice(3,5)
+            }
+
+            let date = `<div id ="background-`+oddeven+`"><h3>`+ data[0][i] +`</h3>`
+            let opponent = `<p>`+ data[1][i] +`</p>`
+            let status = `<p id="status">`+ data[2][i] +`</p></div><br>`
+            
+            if(data[2][i].includes("Top" || "Bottom")){
+                const liveGame = document.getElementById('game-today')
+                liveGame.classList.remove('hidden')
+                liveGame.classList.add('game-today')
+                //Scraping links would be very time consuming to figure out
+                //because of inconsistent setup on Trine site; sometimes there is 
+                //nothing, sometimes there is box score, recap, video, sometimes 
+                //there is a random combination of the three. 
+                //This means the array indices don't line up like all the others.
+                //For now, the alert will only link to trine broadcasting on Vimeo,
+                //meaning only home games will be watchable via the link
+            }
+
+            schedDisplay.innerHTML += date + opponent + status
+        }
+    }).catch(err => console.log(err))
+
+function jumpToGame(){
+    
+    const status = document.querySelectorAll('#status');
+    let statusArr = []
+    //Ideally you would find the next date (starting from today) which exists
+    //in the DOM in the schedule section. However, this approach does the job
+    //with much less code
+
+    for(let i=0; i<status.length; i++){
+        statusArr.push(status[i].innerHTML) 
+        if(status[i].innerHTML.includes("PM" || "AM")){
+            status[i].scrollIntoView(true)
+            window.scrollBy(0, -55)
+            return;   
+        } 
+    } alert('Season is Over! See you next year')
+}
