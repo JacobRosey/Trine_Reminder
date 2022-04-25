5cb741578e5b7b7012d6181e90051add73a81600
JacobRosey <jrosey77@gmail.com>
2022-04-25 13:34:01 -0400

    service worker

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', e => {
     console.log("Install!")
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./src/styles.css", "./src/trine192.png"])
+            return cache.addAll(["./", "./src/trine192.png"])
         })
     )
 })
