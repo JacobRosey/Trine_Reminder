6e4b8d1dd354b50cd289b3e28eb4c0e1a6bdd317
JacobRosey <jrosey77@gmail.com>
2022-04-25 18:24:14 -0400

    delete old app.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', e => {
     console.log("Install!")
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./public/src/trine192.png"])
+            return cache.addAll(["./public/src/trine192.png"])
         })
     )
 })
