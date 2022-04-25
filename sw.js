086ae4f54b0d407a01c8c99f5249392ea9c2a889
JacobRosey <jrosey77@gmail.com>
2022-04-25 14:08:27 -0400

    root dir sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', e => {
+    console.log("Install!")
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "/public/src/trine192.png"])
+        })
+    )
+})
+
