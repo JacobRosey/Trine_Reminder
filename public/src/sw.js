b93cfe4fec395922aaa17885d665007df04e8644
JacobRosey <jrosey77@gmail.com>
2022-04-25 14:09:27 -0400

    delete src/sw.js

--- public/src/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', e => {
-    console.log("Install!")
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "/public/src/trine192.png"])
-        })
-    )
-})
-
