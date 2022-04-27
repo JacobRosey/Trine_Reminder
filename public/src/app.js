5a65322edb02cb39233ff8692fbfe05d3fd23d6e
JacobRosey <jrosey77@gmail.com>
2022-04-27 11:44:53 -0400

    revert

--- public/src/app.js
+++ public/src/app.js
@@ -21,8 +21,8 @@ if("serviceWorker" in navigator) {
 fetch('https://trine-scraper.herokuapp.com/record')
     .then(response => {return response.json()})
     .then(data => {
-        let overall =  `<h3 id="overall-rec">Overall Record: `+data[0]+`</h3>`
-        let conf = `<h3 id="conf-rec">Conference Record: `+data[2]+`</h3>`
+        let overall =  `<h3 id="overall-rec">Overall: `+data[0]+`</h3>`
+        let conf = `<h3 id="conf-rec">Conference: `+data[2]+`</h3>`
         let streak = `<h3 id="streak">Current Streak: `+data[4]+`</h3>`
 
         recDisplay.innerHTML = overall + conf + streak
