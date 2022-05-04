f235cdde672a0e2235f9f3ad162c1c62db6a4e61
JacobRosey <jrosey77@gmail.com>
2022-05-04 14:45:34 -0400

    sw

--- public/sw.js
+++ public/sw.js
@@ -5,6 +5,11 @@ self.addEventListener('install', e => {
             return cache.addAll(["/", "/src/trine192.png"])
         })
     )
+    e.respondWith(
+        caches.match(e.request).then((cacheRes) => {
+            return cacheRes || fetch(e.request)
+        })
+    );
 })
 
 
