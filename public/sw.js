685644a338b8ad00feb3f19adfe29760486787a0
JacobRosey <jrosey77@gmail.com>
2022-05-04 12:47:13 -0400

    syntax error

--- public/sw.js
+++ public/sw.js
@@ -11,12 +11,12 @@ self.addEventListener("fetch", e => {
     console.log(`intercepting fetch request for: ${e.request.url}`)
 
     e.respondWith(
-        caches.match(e.request.then(cacheRes => {
+        caches.match(e.request).then((cacheRes) => {
             if(cacheRes == undefined){
                 console.log(`Missing ${e.request.url}`)
             }
             return cacheRes || fetch(e.request)
-        }))
+        })
     )
 })
 
