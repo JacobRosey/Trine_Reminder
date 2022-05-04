1fb62edfa2f33539f2dc0f7342ebf6b201a79e3a
JacobRosey <jrosey77@gmail.com>
2022-05-04 14:09:27 -0400

    sw

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,3 @@
-//let myCache = `myCache`
-
 self.addEventListener('install', e => {
     console.log("Install!")
     e.waitUntil(
@@ -9,35 +7,4 @@ self.addEventListener('install', e => {
     )
 })
 
-/*
-self.addEventListener("fetch", e => {
-    console.log(`intercepting fetch request for: ${e.request.url}`);
-
-    e.respondWith(
-        caches.match(e.request.then((cacheRes) => {
-            if(cacheRes == undefined){
-                console.log(`Missing ${e.request.url}`)
-            }
-            return cacheRes || fetch(e.request)
-        })
-    )
-)})
-    */
-    /*e.respondWith(
-        caches.match(e.request).then((cacheRes) => {
-            return cacheRes || fetch(e.request)
-            .then(fetchRes => {
-                let type = fetchResponse.headers.get('content-type');
-                if(type && type.match(/^image\//i)){
-                    //save image to cache
-                    console.log(`saved image file ${e.request.url}`);
-                    return caches.open(myCache).then((cache) => {
-                        cache.put(e.request, fetchResponse.clone())
-                        return fetchRes;
-                    })
-                }
-            })
-        })
-    )*/
-//})
 
