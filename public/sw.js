ea3a54a09fa889733980a67615adac75dacad042
JacobRosey <jrosey77@gmail.com>
2022-05-06 12:22:49 -0400

    sw caching

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,19 @@
+var cacheName = 'v1';
+var cacheFiles = [
+    './',
+    './index.html',
+    './src/app.js',
+    './src/styles.css',
+    './index.js',
+    '/src/trine192.png'
+]
+
 self.addEventListener('install', e => {
     console.log("Install!")
     e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["/", "/src/trine192.png"])
+        caches.open(cacheName).then(cache => {
+            console.log("ServiceWorker is caching cacheFiles")
+            return cache.addAll(cacheFiles)
         })
     )
 })
