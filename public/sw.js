ab122b6b5a811ab44e0b5a2fa551a2a7aff82a6c
JacobRosey <jrosey77@gmail.com>
2022-05-06 12:29:45 -0400

    service worker fetch/activate

--- public/sw.js
+++ public/sw.js
@@ -12,10 +12,18 @@ self.addEventListener('install', e => {
     console.log("Install!")
     e.waitUntil(
         caches.open(cacheName).then(cache => {
-            console.log("ServiceWorker is caching cacheFiles")
+            console.log("ServiceWorker is caching cacheFiles");
             return cache.addAll(cacheFiles)
         })
     )
+})
+
+self.addEventListener('activate', (e) => {
+    console.log("Service Worker activated")
+})
+
+self.addEventListener('fetch', (e) => {
+    console.log("Service Worker Fetching", e.request.url)
 })
 
 
