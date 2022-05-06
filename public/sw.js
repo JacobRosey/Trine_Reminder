708fe076a131f4f7ad07d9784d18cdfbb8600ee0
JacobRosey <jrosey77@gmail.com>
2022-05-06 14:53:23 -0400

    still sw paths

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,12 @@
 var cacheName = 'v1';
 var cacheFiles = [
     //One/some of these routes are wrong, fix pls
-    './',
-    './public/index.html',
-    './public/src/app.js',
-    './public/src/styles.css',
-    './index.js',
-    './public/src/trine192.png'
+    '/',
+    'public/index.html',
+    'public/src/app.js',
+    'public/src/styles.css',
+    'index.js',
+    'public/src/trine192.png'
 ]
 
 self.addEventListener('install', e => {
