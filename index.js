c948abe40782c0a0bc9cd57e23d590f99ebd4248
JacobRosey <jrosey77@gmail.com>
2023-01-11 13:47:17 -0500

    space between @ and vs

--- index.js
+++ index.js
@@ -254,7 +254,9 @@ var schedApp = new function(){
                 //Add 'vs.' or '@' to team name
                 if(i==1){
                     if(data[3][j] == 'at'){
-                        data[3][j] = '@';
+                        data[3][j] = '@ ';
+                    } else{
+                        data[3][j] = 'vs. '
                     }
                     data[i][j] = data[3][j].concat(data[i][j])
                 }
